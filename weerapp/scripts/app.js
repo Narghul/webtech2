e2399f00f33d2dfd052b9b990d003ef00cc946f3
= <silent785@hotmail.com>
2014-03-19 20:44:24 +0100

    Rewritten whole code with prototypes

--- weerapp/scripts/app.js
+++ weerapp/scripts/app.js
@@ -9,142 +9,21 @@ $(function() {
 });
 
 var Weather = function(){
-      var skycons = new Skycons({"color": "white"});
-
-
-
-
-
+    //var skycons = new Skycons({"color": "white"});
     var huidigePositie = '';
     var jsonResponse = '';
     var locatieResponse = '';
 
-    function unixToHuman(tijd){
+    
+}
+Weather.prototype.skycons = new Skycons({"color": "white"});
+
+Weather.prototype.unixToHuman = function(tijd){
         var cachedTijd = new Date(tijd*1000);
         return cachedTijd;
 
-    }
-
-    function getLocation()
-      {
-      if (navigator.geolocation)
-        {
-        navigator.geolocation.getCurrentPosition(showPosition, error);
-        }
-      else{this.x.innerHTML = "Geolocation is not supported by this browser.";}
-      }
-    function showPosition(position)
-      {
-      //this.x.innerHTML = "Latitude: " + position.coords.latitude +
-      //"<br>Longitude: " + position.coords.longitude;
-      huidigePositie = position;
-      //console.log("huidigePositie wordt positie" + huidigePositie);
-      //localstorage.setItem("pos", huidigePositie);
-    doLocation();
-
-      if(localStorage.jsonitem == null){
-              doAjax();
-
-
-      }else{
-
-        var jsonResponse = localStorage.getItem("jsonitem");
-                  console.log("weer is gecached");
-          var locatieResponse = localStorage.getItem("locatie");
-                        var nu = new Date();
-          var cachedTijd = new Date(JSON.parse(jsonResponse).currently.time*1000);
-         // console.log(cachedTijd + "cached tijd");
-
-          var t1 = new Date(0, 0, 0, cachedTijd.getHours(), cachedTijd.getMinutes(), 0, 0);
-          var t2 = new Date(0, 0, 0, nu.getHours(), nu.getMinutes(), 0, 0);
-var dif = t2.getTime() - t1.getTime();
-
-var Seconds_from_T1_to_T2 = dif / 1000;
-var Seconds_Between_Dates = Math.abs(Seconds_from_T1_to_T2);
-
-
-
-
-          if( Seconds_Between_Dates >= 3600){
-                console.log("weer update nodig");
-              var nu = new Date().getTime();
-              //console.log(nu);
-              //console.log(JSON.parse(jsonResponse).currently.time);
-
-              doAjax();
-          }else{
-              console.log("Geen weerupdate nodig");
-
-            gebruikResponse(JSON.parse(jsonResponse));
-
-
-
-          }
-
-
-
-  }
-      }
-
-      function error(){
-        console.log("error met het vinden van locatie")
-      }
-    getLocation();
-            function doLocation(){
-
-    var request = $.ajax({
-        url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + huidigePositie.coords.latitude + ',' + huidigePositie.coords.longitude + '&sensor=true',
-        //api key = &key=AIzaSyC39ss-ApCy2Pt6_VuXgjpcDOe_NW9rGJM
-        type: 'GET',
-        dataType: 'json',
-        //data: {param1: 'value1'},
-    })
-    .done(function() {
-        console.log("[GMaps]success");
-        locatieResponse = request.responseJSON;
-        //console.log(locatieResponse);
-        localStorage.setItem("locatie", JSON.stringify(locatieResponse));
-
-
-    })
-    .fail(function() {
-        console.log("error");
-    })
-    .always(function() {
-        console.log("complete");
-
-
-    });
-            }
-function doAjax(){
-
-    var jqxhr = $.ajax({
-        url: 'https://api.forecast.io/forecast/bd57f534e65a91f03c3d1f82735e435a/' + huidigePositie.coords.latitude + ',' + huidigePositie.coords.longitude+'?units=auto',
-        //url: 'https://api.forecast.io/forecast/bd57f534e65a91f03c3d1f82735e435a/37.8267,-122.423',
-        type: 'GET',
-        dataType: 'jsonp',
-        //data: {param1: 'value1'},
-    })
-    .done(function() {
-        console.log("success");
-        jsonResponse = jqxhr.responseJSON;
-        gebruikResponse(jsonResponse);
-        localStorage.setItem("jsonitem", JSON.stringify(jsonResponse));
-
-
-    })
-    .fail(function() {
-        console.log("error");
-    })
-    .always(function() {
-        console.log("complete");
-
-
-    });
-
-
 }
-function getDagFromNumber(dag){
+Weather.prototype.getDagFromNumber = function(dag){
     switch(dag){
             case 1:
                 return "Mon";
@@ -169,8 +48,91 @@ function getDagFromNumber(dag){
             break;
 }
 }
-function gebruikResponse(jsonResponse){
-    var date = new Date(jsonResponse.currently.time*1000);
+Weather.prototype.doAjax = function(huidigePositie){
+    console.log("doAjax "+Weather.prototype.huidigePositie);
+
+    var jqxhr = $.ajax({
+        url: 'https://api.forecast.io/forecast/bd57f534e65a91f03c3d1f82735e435a/' + Weather.prototype.huidigePositie.coords.latitude + ',' + Weather.prototype.huidigePositie.coords.longitude+'?units=auto',
+        //url: 'https://api.forecast.io/forecast/bd57f534e65a91f03c3d1f82735e435a/37.8267,-122.423',
+        type: 'GET',
+        dataType: 'jsonp',
+        //data: {param1: 'value1'},
+    })
+    .done(function() {
+        console.log("success");
+        Weather.prototype.jsonResponse = jqxhr.responseJSON;
+        Weather.prototype.gebruikResponse();
+        localStorage.setItem("jsonitem", JSON.stringify(Weather.prototype.jsonResponse));
+
+
+    })
+    .fail(function() {
+        console.log("error");
+    })
+    .always(function() {
+        console.log("complete");
+
+
+    });
+
+
+}
+Weather.prototype.getLocation = function(){
+      if (navigator.geolocation)
+        {
+        navigator.geolocation.getCurrentPosition(showPosition, error);
+        }else{
+            this.x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+
+      function error(){
+        console.log("error met het vinden van locatie")
+      }
+      function showPosition(position){
+      if(position != null){
+            Weather.prototype.huidigePositie = position;
+
+      }
+      
+        console.log(Weather.prototype.huidigePositie);
+      if(localStorage.jsonitem == null){
+        Weather.prototype.doAjax(Weather.prototype.huidigePositie);
+      }else{
+        console.log(Weather.prototype.jsonResponse);
+        Weather.prototype.jsonResponse = JSON.parse(localStorage.getItem("jsonitem"));
+                  console.log("weer is gecached");
+          this.locatieResponse = localStorage.getItem("locatie");
+                        var nu = new Date();
+          var cachedTijd = new Date(Weather.prototype.jsonResponse.currently.time*1000);
+         // console.log(cachedTijd + "cached tijd");
+
+          var t1 = new Date(0, 0, 0, cachedTijd.getHours(), cachedTijd.getMinutes(), 0, 0);
+          var t2 = new Date(0, 0, 0, nu.getHours(), nu.getMinutes(), 0, 0);
+            var dif = t2.getTime() - t1.getTime();
+
+            var Seconds_from_T1_to_T2 = dif / 1000;
+            var Seconds_Between_Dates = Math.abs(Seconds_from_T1_to_T2);
+
+         if( Seconds_Between_Dates >= 3600){
+                console.log("weer update nodig");
+              var nu = new Date().getTime();
+              //console.log(nu);
+              //console.log(JSON.parse(jsonResponse).currently.time);
+             Weather.prototype.doAjax(Weather.prototype.huidigePositie);
+          }else{
+              console.log("Geen weerupdate nodig");
+            Weather.prototype.gebruikResponse(Weather.prototype.jsonResponse);
+
+
+
+          }
+
+  }
+      }
+}
+Weather.prototype.gebruikResponse = function(){
+    console.log(Weather.prototype.jsonResponse.currently.time);
+    var date = new Date(Weather.prototype.jsonResponse.currently.time*1000);
 // hours part from the timestamp
 var hours = date.getHours();
 // minutes part from the timestamp
@@ -180,15 +142,14 @@ var seconds = date.getSeconds();
 
 
 var formattedTime = hours + ':' + minutes + ':' + seconds;
-    var voorspelling = jsonResponse.daily.data;
+    var voorspelling = Weather.prototype.jsonResponse.daily.data;
     var voorspellingDatum = [];
     for(var i = 0; i<4; i++){
-        var p = unixToHuman(new Date(voorspelling[i].time));
+        var p = this.unixToHuman(new Date(voorspelling[i].time));
         var d = new Date(2014, 1+p.getMonth(), p.getDate())
         voorspellingDatum.push(d);
 
     }
-    //locatieresponse.results[0].address_components[3].long_name +
     if(localStorage.getItem("locatie") == null){
 
     }else{
@@ -198,33 +159,28 @@ var formattedTime = hours + ':' + minutes + ':' + seconds;
 
     $("#locatie").append(locatieResponse["results"][4].formatted_address);
     }
-    var conditie = jsonResponse.currently.icon;
+    var conditie = Weather.prototype.jsonResponse.currently.icon;
     //mogelijke condities van forecast.io clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
-    getIcon(conditie);
-    getColor(conditie);
-    $("#currentTemp").append(Math.round(jsonResponse.currently.temperature) + '°C');
-            $("#beschrijving").append('<br /> ' +jsonResponse.currently.summary).append('<br />').append('<br />').append(jsonResponse.currently.windSpeed + ' km/h').append("<br />" + formattedTime + "<br />" + jsonResponse.hourly.summary);
+    Weather.prototype.getIcon(conditie);
+    Weather.prototype.getColor(conditie);
+    $("#currentTemp").append(Math.round(Weather.prototype.jsonResponse.currently.temperature) + '°C');
+            $("#beschrijving").append('<br /> ' +Weather.prototype.jsonResponse.currently.summary).append('<br />').append('<br />').append(Weather.prototype.jsonResponse.currently.windSpeed + ' km/h').append("<br />" + formattedTime + "<br />" + Weather.prototype.jsonResponse.hourly.summary);
 
     for(var i = 1; i<4; i++){
-        $("#f"+i).append(getDagFromNumber(unixToHuman(voorspelling[i].time).getDay()) + ' ' + voorspellingDatum[i].getDate() + '/'+ voorspellingDatum[i].getMonth() +'<br />' +
+        $("#f"+i).append(Weather.prototype.getDagFromNumber(Weather.prototype.unixToHuman(voorspelling[i].time).getDay()) + ' ' + voorspellingDatum[i].getDate() + '/'+ voorspellingDatum[i].getMonth() +'<br />' +
 voorspelling[i].temperatureMax +'</div>');
 
     }
     $("#forecast").append('<div class="dummy"></div>');
 
-    //console.log(getDagFromNumber(unixToHuman(voorspelling[0].time).getDay()));
-
-
-
-
 }
-function getIcon(cond){
+Weather.prototype.getIcon = function(cond){
     switch(cond){
      case "clear-day":
-skycons.add(document.getElementById("links"), Skycons.CLEAR_DAY);
+this.skycons.add(document.getElementById("links"), Skycons.CLEAR_DAY);
 break;
     case "clear-night":
-skycons.add(document.getElementById("links"), Skycons.CLEAR_NIGHT);
+this.skycons.add(document.getElementById("links"), Skycons.CLEAR_NIGHT);
 break;
     case "rain":
 skycons.add(document.getElementById("links"), Skycons.RAIN);
@@ -260,10 +216,10 @@ skycons.add(document.getElementById("links"), Skycons.CLEAR_DAY);
             break;
 
     }
-          skycons.play();
+          this.skycons.play();
 
 }
-function getColor(bg){
+Weather.prototype.getColor = function(bg){
     switch(bg){
      case "clear-day":
             $( "body" ).css("background-color", "#516161");
@@ -301,9 +257,12 @@ function getColor(bg){
             break;
         }
 }
-}
+
+
+
 
 var weer = new Weather();
+weer.getLocation();
 
 
 
