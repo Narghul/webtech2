665a59cbdfca7e9a2b2687836bde7c9795bea6c8
= <silent785@hotmail.com>
2014-04-03 17:24:31 +0200

    ADD Question class to avoid 'DRY'

--- ask/public/js/allquestions.js
+++ ask/public/js/allquestions.js
@@ -1,7 +1,6 @@
 var client = new Faye.Client('http://localhost:3000/faye');
 console.log("AllQuestions_Client Connected");
 var questionList=[];
-console.log(client);
 var Question = function (vote, question){
     this.question = question;
     this.vote = 0;
@@ -11,9 +10,8 @@ Question.prototype.addVote= function() {
     this.vote++;
 }
 client.subscribe("/question", function(message) {
-    console.log('Got a message: '+message.answered);
     q=new Question(message.question, message.vote);
     console.log(message);
     
-    $("#questions").append(message.question+"<a href='#' class='votes' data-id='"+questionList.length+"'>"+message.vote+"</a><br />");
+    $("#questions").append(message.question+ " "+"<a href='#' class='votes'>"+message.vote+"</a><br />");
 });
--- ask/public/js/ask-client.js
+++ ask/public/js/ask-client.js
@@ -1,14 +1,6 @@
 var client = new Faye.Client("http://localhost:3000/faye");
 console.log("Client connected");
 
-var Question = function (vote, question){
-    this.question = question;
-    this.vote = 0;
-       
-}
-Question.prototype.addVote= function() {
-    this.vote++;
-}
 $("input[type=submit]").click(publishQuestion);
 
 function publishQuestion() {
