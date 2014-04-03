c5b7d89d5b887b3180afc01dff4cf754aa266e65
= <silent785@hotmail.com>
2014-04-03 18:55:30 +0200

    ADD ability to vote on questions

--- ask/public/js/allquestions.js
+++ ask/public/js/allquestions.js
@@ -1,17 +1,15 @@
 var client = new Faye.Client('http://localhost:3000/faye');
 console.log("AllQuestions_Client Connected");
 var questionList=[];
-var Question = function (vote, question){
-    this.question = question;
-    this.vote = 0;
-       
-}
-Question.prototype.addVote= function() {
-    this.vote++;
-}
 client.subscribe("/question", function(message) {
-    q=new Question(message.question, message.vote);
+   var q=new Question(message.question, message.vote, message.naam);
     console.log(message);
-    
-    $("#questions").append(message.question+ " "+"<a href='#' class='votes'>"+message.vote+"</a><br />");
+    var l = questionList.length;
+    $("#questions").append("<div class='question' id='q" + l++ +"'>" + message.question+ " "+"<a href='#' class='votes'>"+message.vote+"</a></div><br />");
+    var i = 1;
+    $(".question").click(function(event){
+        var currentVotes = event.currentTarget.children[0].innerHTML;
+        event.currentTarget.children[0].innerHTML = String(Number(currentVotes) + 1);
+    });
+    questionList.push(q);
 });
--- ask/public/js/ask-client.js
+++ ask/public/js/ask-client.js
@@ -2,11 +2,13 @@ var client = new Faye.Client("http://localhost:3000/faye");
 console.log("Client connected");
 
 $("input[type=submit]").click(publishQuestion);
+var chance = new Chance();
 
 function publishQuestion() {
     console.log("clicked");
     var questionField = $("input[name=question-txt]").val();
-    var q= new Question(0, questionField);
+    var naam = chance.name();
+    var q= new Question(0, questionField, naam);
     $("input[name=question-txt]").val("");
     console.log(q);
     client.publish("/question", q);
